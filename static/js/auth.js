579722e3344656f5f146e508067c4c9ed7e5877b
Dafe-akaka <dafe.akaka123@gmail.com>
2022-02-21 19:47:25 +0000

    instant login

--- static/js/auth.js
+++ static/js/auth.js
@@ -1,12 +1,13 @@
 const jwt_decode = require('jwt-decode');
 
 
+
 const requestSignup= async (e) => {
     e.preventDefault();
+    const form = e.target
     const username = e.target.username.value
     const email = e.target.Semail.value
     const password = e.target.Spassword.value
-
     try {
         const postData = {
             username: username,
@@ -22,18 +23,45 @@ const requestSignup= async (e) => {
         const data = await res.json()
         if (data.err){ throw Error(data.err) }
         console.log('user created')
-        requestLogin(e);
+        instantLogin(e);
         form.reset()
     } catch (err) {
         console.warn(err);
     }
 }
 
+const instantLogin = async (e) => {
+    e.preventDefault();
+    const email = e.target.Semail.value
+    const password = e.target.Spassword.value
+    try {
+        const postData = {
+            email: email,
+            password: password
+        }
+        const options = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(postData),
+        };
+        const res = await fetch(`http://localhost:3000/auth/login`, options);
+        const data = await res.json();
+        if (!data.success) {
+        throw new Error("Login not authorised");
+        }
+        form.reset()
+        login(data.token);
+  } catch (err) {
+        console.warn(err);
+  }
+}
+
 const requestLogin = async (e) => {
   e.preventDefault();
   console.log(e.target)
-  const email =  e.target.Semail.value || e.target.email.value
-  const password =  e.target.Spassword.value || e.target.password.value
+  const form = e.target
+  let email = e.target.email.value 
+  let password = e.target.password.value 
   try {
         const postData = {
             email: email,
