f13699f418a2a4b4d3f4cbabcd4d5fec36d246cd
Dafe-akaka <dafe.akaka123@gmail.com>
2022-02-21 17:42:16 +0000

    restructure

--- static/js/auth.js
+++ static/js/auth.js
@@ -1,9 +1,39 @@
 const jwt_decode = require('jwt-decode');
 
+
+const requestSignup= async (e) => {
+    e.preventDefault();
+    const username = e.target.username.value
+    const email = e.target.Semail.value
+    const password = e.target.Spassword.value
+
+    try {
+        const postData = {
+            username: username,
+            email: email,
+            password: password
+        }
+        const options = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(postData)
+        }
+        const res = await fetch(`http://localhost:3000/auth/register`, options)
+        const data = await res.json()
+        if (data.err){ throw Error(data.err) }
+        console.log('user created')
+        requestLogin(e);
+        form.reset()
+    } catch (err) {
+        console.warn(err);
+    }
+}
+
 const requestLogin = async (e) => {
   e.preventDefault();
-  const email = e.target.email.value
-  const password = e.target.password.value
+  console.log(e.target)
+  const email =  e.target.Semail.value || e.target.email.value
+  const password =  e.target.Spassword.value || e.target.password.value
   try {
         const postData = {
             email: email,
@@ -26,32 +56,7 @@ const requestLogin = async (e) => {
   }
 };
 
-const requestSignup= async (e) => {
-    e.preventDefault();
-    const username = e.target.username.value
-    const email = e.target.Semail.value
-    const password = e.target.Spassword.value
 
-    try {
-        const postData = {
-            username: username,
-            email: email,
-            password: password
-        }
-        const options = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(postData)
-        }
-        const res = await fetch(`http://localhost:3000/auth/signup`, options)
-        const data = await res.json()
-        if (data.err){ throw Error(data.err) }
-        requestLogin(e);
-        form.reset()
-    } catch (err) {
-        console.warn(err);
-    }
-}
 function login(token){
     const user = jwt_decode(token);
     localStorage.setItem("token", token);
