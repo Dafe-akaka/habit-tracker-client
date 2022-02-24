e783bbb4d9578f438c26786df1f7bc312d1c3906
giacomo <88339527+gi-ba-bu@users.noreply.github.com>
2022-02-24 22:51:50 +0100

    requests.js tests

---
+++ tests/requests.spec.js
@@ -0,0 +1,45 @@
+/**
+* @jest-environment jsdom
+*/
+const { getAllHabits, createHabit } = require("../static/js/requests");
+
+global.fetch = require('jest-fetch-mock');
+
+describe('api', () => {
+
+    afterEach(() => {
+        fetch.resetMocks();
+    });
+
+    describe('requests', () => {
+        
+        describe('get requests ', () => {
+
+            test('it makes a get request', () => {
+                getAllHabits();
+                expect(fetch.mock.calls.length).toEqual(1) 
+                expect(fetch.mock.calls[0][0]).toContain('habits/habits')
+            })
+        });
+
+        describe('post requests', () => {
+
+            test('it makes a post request', () => {
+                
+                const fakeSubmitEvent = {
+                    preventDefault: jest.fn(),
+                    target: {
+                       frequency: {value: 10}, // title: {value: "Homer moment"},
+                       habit: {value: "running"} ,//message: {value: "Forgetting to add a script tag"},
+                       }
+                };
+
+                createHabit(fakeSubmitEvent);
+                expect(fetch.mock.calls[0][1]).toHaveProperty('method', 'POST');
+                expect(fetch.mock.calls[0][1]).toHaveProperty('body', JSON.stringify({habit: "running", frequency: 10}))
+            })
+        })
+
+    })
+
+})
